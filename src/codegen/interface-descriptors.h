9e390b5adcd264b80bd1bc2c47c698e3fc456fbd
Reza Yazdani <ryazdani@futurewei.com>
2020-03-20 17:51:15 -0700

    Changes to register for RISCV

--- src/codegen/interface-descriptors.h
+++ src/codegen/interface-descriptors.h
@@ -359,9 +359,7 @@ class V8_EXPORT_PRIVATE CallInterfaceDescriptor {
     InitializePlatformSpecific(data);
     InitializePlatformIndependent(data);
     DCHECK(data->IsInitialized());
-		// Following line causes mksnapshout failes. Come back to it after
-		// implementation of floating point operations and uncomment it. R.Y.
-    //DCHECK(CheckFloatingPointParameters(data));
+    DCHECK(CheckFloatingPointParameters(data));
   }
 };
 
