ed6deeeabc2a19671a9fda3f228e42e18628fd01
Brice Dobry <brice.dobry@futurewei.com>
2020-06-04 08:11:01 -0700

    Cleanup MIPS comments in RISC-V code
    Either fixed or opened issues for remaining questions.
    
    Resolves #56

--- src/codegen/reloc-info.cc
+++ src/codegen/reloc-info.cc
@@ -330,8 +330,8 @@ bool RelocInfo::OffHeapTargetIsCodedSpecially() {
   return false;
 #elif defined(V8_TARGET_ARCH_IA32) || defined(V8_TARGET_ARCH_MIPS) || \
     defined(V8_TARGET_ARCH_MIPS64) || defined(V8_TARGET_ARCH_PPC) ||  \
-    defined(V8_TARGET_ARCH_PPC64) || defined(V8_TARGET_ARCH_S390) || defined(V8_TARGET_ARCH_RISCV)
-  //FIXME: RISCV porting confirm the value
+    defined(V8_TARGET_ARCH_PPC64) || defined(V8_TARGET_ARCH_S390) ||  \
+    defined(V8_TARGET_ARCH_RISCV)
   return true;
 #endif
 }
--- src/snapshot/deserializer.h
+++ src/snapshot/deserializer.h
@@ -32,7 +32,6 @@ class Object;
     defined(V8_TARGET_ARCH_PPC) || defined(V8_TARGET_ARCH_S390) ||    \
     defined(V8_TARGET_ARCH_PPC64) || defined(V8_TARGET_ARCH_RISCV) || \
     V8_EMBEDDED_CONSTANT_POOL
-    //FIXME: RISCV porting check the value
 #define V8_CODE_EMBEDS_OBJECT_POINTER 1
 #else
 #define V8_CODE_EMBEDS_OBJECT_POINTER 0
