fc971b996a74d7eee717e151eb41441715b2be78
Peng Wu <peng.wu@futurewei.com>
2020-08-06 08:00:07 -0700

    Cleanup of Lsa32 and Lsa64 from instruction selection
    kRiscvLsa32 and kRiscvLsa64 are legacy from MIPS where MIPS has a single
    instruction to do address calculation w/ a scaling factor (i.e., pattern
    "rt + rs << sa"). This pattern is common in calculating the address of
    an array element (i.e., `addrOf(a[i])`).
    
    Since RISCV does not provide single instruction for this pattern, removed
    kRiscvLsa32 and kRiscvLsa64 from instruction selection. However, macro-
    assembler API Lsa64() is used in various places to calculate addresses of
    array elements. Since this pattern is common, we keep Lsa64() and rename it
    to CalcScaledAddress().

--- src/compiler/backend/riscv64/instruction-scheduler-riscv64.cc
+++ src/compiler/backend/riscv64/instruction-scheduler-riscv64.cc
@@ -57,7 +57,6 @@ int InstructionScheduler::GetTargetInstructionFlags(
     case kRiscvDivD:
     case kRiscvDivS:
     case kRiscvDivU32:
-    case kRiscvLsa64:
     case kRiscvMod64:
     case kRiscvModU64:
     case kRiscvMul64:
@@ -242,7 +241,6 @@ int InstructionScheduler::GetTargetInstructionFlags(
     case kRiscvI8x16Abs:
     case kRiscvI8x16BitMask:
     case kRiscvIns32:
-    case kRiscvLsa32:
     case kRiscvMaxD:
     case kRiscvMaxS:
     case kRiscvMinD:
@@ -691,11 +689,6 @@ int MovzLatency() { return 1; }
 
 int MovnLatency() { return 1; }
 
-int Lsa64Latency() {
-  // Estimated max.
-  return Add64Latency() + 1;
-}
-
 int CallLatency() {
   // Estimated.
   return Add64Latency(false) + Latency::BRANCH + 5;
@@ -710,7 +703,7 @@ int SmiUntagLatency() { return 1; }
 
 int PrepareForTailCallLatency() {
   // Estimated max.
-  return 2 * (Lsa64Latency() + Add64Latency(false)) + 2 + Latency::BRANCH +
+  return 2 * (Add64Latency() + 1 + Add64Latency(false)) + 2 + Latency::BRANCH +
          Latency::BRANCH + 2 * Sub64Latency(false) + 2 + Latency::BRANCH + 1;
 }
 
@@ -1234,9 +1227,6 @@ int InstructionScheduler::GetInstructionLatency(const Instruction* instr) {
       return Mod64Latency();
     case kRiscvModU64:
       return Modu64Latency();
-    case kRiscvLsa64:
-    case kRiscvLsa32:
-      return Lsa64Latency();
     case kRiscvAnd:
       return AndLatency(instr->InputAt(1)->IsRegister());
     case kRiscvAnd32: {
