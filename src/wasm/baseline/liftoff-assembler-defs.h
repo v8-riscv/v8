52f72168a7addaaa3eb4a621480ac2923dc64dc7
Peng Wu <peng.wu@futurewei.com>
2020-08-11 08:34:18 -0700

    Fix kPushedFpRegs to be consistent w/ kLiftoffAssemblerFpCacheRegs

--- src/wasm/baseline/liftoff-assembler-defs.h
+++ src/wasm/baseline/liftoff-assembler-defs.h
@@ -71,9 +71,13 @@ constexpr RegList kLiftoffAssemblerFpCacheRegs = CPURegister::ListOf(
 
 #elif V8_TARGET_ARCH_RISCV64
 
+// Any change of kLiftoffAssemblerGpCacheRegs also need to update
+// kPushedGpRegs in frame-constants-riscv64.h
 constexpr RegList kLiftoffAssemblerGpCacheRegs =
-    Register::ListOf(a0, a1, a2, a3, a4, a5, a6, a7, t0, t1, t2, s7, t4);
+    Register::ListOf(a0, a1, a2, a3, a4, a5, a6, a7, t0, t1, t2, s7);
 
+// Any change of kLiftoffAssemblerGpCacheRegs also need to update
+// kPushedFpRegs in frame-constants-riscv64.h
 constexpr RegList kLiftoffAssemblerFpCacheRegs =
     DoubleRegister::ListOf(ft0, ft1, ft2, ft3, ft4, ft5, ft6, ft7, fa0, fa1,
                            fa2, fa3, fa4, fa5, fa6, fa7, ft8, ft9, ft10, ft11);
