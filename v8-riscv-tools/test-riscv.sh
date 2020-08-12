817bed6af9e42a88c58c35dc95235bd26b2b45ab
Peng Wu <peng.wu@futurewei.com>
2020-08-12 11:58:26 -0700

    Add wasm-js test to CI

--- v8-riscv-tools/test-riscv.sh
+++ v8-riscv-tools/test-riscv.sh
@@ -5,6 +5,6 @@ DIR=$(dirname "$SCRIPT")
 
 $DIR/../tools/run-tests.py -p verbose --report --outdir=out/riscv64.sim cctest
 $DIR/../tools/run-tests.py -p verbose --report --outdir=out/riscv64.sim unittests
-$DIR/../tools/run-tests.py -p verbose --report --outdir=out/riscv64.sim wasm-api-tests
+$DIR/../tools/run-tests.py -p verbose --report --outdir=out/riscv64.sim wasm-api-tests wasm-js
 $DIR/../tools/run-tests.py -p verbose --report --outdir=out/riscv64.sim mjsunit
 $DIR/../tools/run-tests.py -p verbose --report --outdir=out/riscv64.sim intl message debugger inspector mkgrokdump
