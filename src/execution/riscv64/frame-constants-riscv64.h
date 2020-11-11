42c9e6fd07f3a755edc75eaaf0dc75e836fc4fe7
Brice Dobry <brice.dobry@futurewei.com>
2020-11-11 12:36:09 -0500

    Fix include guards
    Proper naming is required by upstream lint checkers.

--- src/execution/riscv64/frame-constants-riscv64.h
+++ src/execution/riscv64/frame-constants-riscv64.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef V8_EXECUTION_RISCV_FRAME_CONSTANTS_RISCV_H_
-#define V8_EXECUTION_RISCV_FRAME_CONSTANTS_RISCV_H_
+#ifndef V8_EXECUTION_RISCV64_FRAME_CONSTANTS_RISCV64_H_
+#define V8_EXECUTION_RISCV64_FRAME_CONSTANTS_RISCV64_H_
 
 #include "src/base/bits.h"
 #include "src/base/macros.h"
@@ -83,4 +83,4 @@ class WasmDebugBreakFrameConstants : public TypedFrameConstants {
 }  // namespace internal
 }  // namespace v8
 
-#endif  // V8_EXECUTION_RISCV_FRAME_CONSTANTS_RISCV_H_
+#endif  // V8_EXECUTION_RISCV64_FRAME_CONSTANTS_RISCV64_H_
