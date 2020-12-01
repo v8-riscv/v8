535f1c043fcd6d178a85d578cee64f7ee680a3b0
luyahan <yahan@iscas.ac.cn>
2020-12-01 12:09:42 +0900

    Fix CheckedFloat64ToInt64 on riscv64

--- test/cctest/compiler/test-run-machops.cc
+++ test/cctest/compiler/test-run-machops.cc
@@ -4194,7 +4194,7 @@ TEST(RunTruncateFloat32ToInt32) {
 #if V8_TARGET_ARCH_IA32 || V8_TARGET_ARCH_X64
         CHECK_FLOAT_EQ(std::numeric_limits<int32_t>::min(), m.Call(i));
 #elif V8_TARGET_ARCH_ARM64 || V8_TARGET_ARCH_ARM || V8_TARGET_ARCH_S390X || \
-    V8_TARGET_ARCH_PPC || V8_TARGET_ARCH_PPC64
+    V8_TARGET_ARCH_PPC || V8_TARGET_ARCH_PPC64 || V8_TARGET_ARCH_RISCV64
         CHECK_FLOAT_EQ(std::numeric_limits<int32_t>::max(), m.Call(i));
 #endif
       } else {
@@ -4204,6 +4204,8 @@ TEST(RunTruncateFloat32ToInt32) {
         CHECK_FLOAT_EQ(std::numeric_limits<int32_t>::min(), m.Call(i));
 #elif V8_TARGET_ARCH_ARM64 || V8_TARGET_ARCH_ARM
         CHECK_FLOAT_EQ(0, m.Call(i));
+#elif V8_TARGET_ARCH_RISCV64
+        CHECK_FLOAT_EQ(std::numeric_limits<int32_t>::max(), m.Call(i));
 #endif
       }
     }
