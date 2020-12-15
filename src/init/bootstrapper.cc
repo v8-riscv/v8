fe3fc149e3660fcd3b92e252dbe4c3b5dc1b5e20
Brice Dobry <brice.dobry@futurewei.com>
2020-12-15 13:40:54 -0500

    Revert formatting changes to unrelated files

--- src/init/bootstrapper.cc
+++ src/init/bootstrapper.cc
@@ -4204,9 +4204,9 @@ void Genesis::InitializeCallSiteBuiltins() {
   FunctionInfo infos[] = {
       {"getColumnNumber", Builtins::kCallSitePrototypeGetColumnNumber},
       {"getEnclosingColumnNumber",
-       Builtins::kCallSitePrototypeGetEnclosingColumnNumber},
+        Builtins::kCallSitePrototypeGetEnclosingColumnNumber},
       {"getEnclosingLineNumber",
-       Builtins::kCallSitePrototypeGetEnclosingLineNumber},
+        Builtins::kCallSitePrototypeGetEnclosingLineNumber},
       {"getEvalOrigin", Builtins::kCallSitePrototypeGetEvalOrigin},
       {"getFileName", Builtins::kCallSitePrototypeGetFileName},
       {"getFunction", Builtins::kCallSitePrototypeGetFunction},
