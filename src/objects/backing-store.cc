9feb22d0d94fabb80edf6ea0fa8703f99e41430b
Brice Dobry <brice.dobry@futurewei.com>
2020-12-16 11:36:25 -0500

    Correct address space limit for RISC-V (Sv39)

--- src/objects/backing-store.cc
+++ src/objects/backing-store.cc
@@ -25,19 +25,19 @@ namespace v8 {
 namespace internal {
 
 namespace {
-#if V8_TARGET_ARCH_MIPS64 || V8_TARGET_ARCH_RISCV64
-// FIXME(RISCV): Check this value
+#if V8_TARGET_ARCH_MIPS64
 // MIPS64 has a user space of 2^40 bytes on most processors,
 // address space limits needs to be smaller.
 constexpr size_t kAddressSpaceLimit = 0x8000000000L;  // 512 GiB
+#elif V8_TARGET_ARCH_RISCV64
+// RISC-V64 has a user space of 256GB on the Sv39 scheme.
+constexpr size_t kAddressSpaceLimit = 0x4000000000L;  // 256 GiB
+#elif V8_TARGET_ARCH_RISCV
+#error RISCV(32) architecture not supported
 #elif V8_TARGET_ARCH_64_BIT
 constexpr size_t kAddressSpaceLimit = 0x10100000000L;  // 1 TiB + 4 GiB
 #else
 constexpr size_t kAddressSpaceLimit = 0xC0000000;  // 3 GiB
-#endif
-
-#if V8_TARGET_ARCH_RISCV
-#error RISCV(32) architecture not supported
 #endif
 
 constexpr uint64_t kNegativeGuardSize = uint64_t{2} * GB;
