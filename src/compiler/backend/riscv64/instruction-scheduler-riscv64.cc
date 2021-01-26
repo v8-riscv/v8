c33320d17b609e0def5d7d2038f07a780f97db62
Brice Dobry <brice.dobry@futurewei.com>
2021-01-26 17:46:08 -0500

    Remove kArchTailCallCodeObjectFromJSFunction
    Port of b8744aa2.

--- src/compiler/backend/riscv64/instruction-scheduler-riscv64.cc
+++ src/compiler/backend/riscv64/instruction-scheduler-riscv64.cc
@@ -1096,14 +1096,7 @@ int InstructionScheduler::GetInstructionLatency(const Instruction* instr) {
     case kArchCallCodeObject:
     case kArchCallWasmFunction:
       return CallLatency();
-    case kArchTailCallCodeObjectFromJSFunction:
     case kArchTailCallCodeObject: {
-      int latency = 0;
-      if (instr->arch_opcode() == kArchTailCallCodeObjectFromJSFunction) {
-        latency = AssemblePopArgumentsAdoptFrameLatency();
-      }
-      return latency + JumpLatency();
-    }
     case kArchTailCallWasm:
     case kArchTailCallAddress:
       return JumpLatency();
