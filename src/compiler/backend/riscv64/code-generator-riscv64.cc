0227458ec4b66291d8030f184b716719fc2091a6
luyahan <yahan@iscas.ac.cn>
2021-04-07 14:26:51 +0900

    Port 064ca18ca284f7115e62501ac643ef8e6c614f76

--- src/compiler/backend/riscv64/code-generator-riscv64.cc
+++ src/compiler/backend/riscv64/code-generator-riscv64.cc
@@ -2283,10 +2283,6 @@ void CodeGenerator::AssembleArchTableSwitch(Instruction* instr) {
   });
 }
 
-void CodeGenerator::AssembleArchSelect(Instruction* instr,
-                                       FlagsCondition condition) {
-  UNIMPLEMENTED();
-}
 
 void CodeGenerator::FinishFrame(Frame* frame) {
   auto call_descriptor = linkage()->GetIncomingDescriptor();
